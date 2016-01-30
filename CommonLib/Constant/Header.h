113fd644eae9afc4c39c91339a5b5a453a3dc288
yurongde <359067441@qq.com>
2016-01-30 09:45:08 +0800

    add header file

---
+++ CommonLib/Constant/Header.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  MainAppProgram
+//
+//  Created by yurongde on 16/1/30.
+//  Copyright © 2016年 yurongde. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#define ScreenHeight 340
+
+#endif /* Header_h */
