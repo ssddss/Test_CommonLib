3945a0dd52ec0312d80ed4a2034fa77a62cecc7d
yurongde <359067441@qq.com>
2016-01-30 10:13:09 +0800

    add color

---
+++ CommonLib/Constant/Color.h
@@ -0,0 +1,14 @@
+//
+//  Color.h
+//  MainAppProgram
+//
+//  Created by yurongde on 16/1/30.
+//  Copyright © 2016年 yurongde. All rights reserved.
+//
+
+#ifndef Color_h
+#define Color_h
+
+#define WhiteColr 0xffffff
+
+#endif /* Color_h */
