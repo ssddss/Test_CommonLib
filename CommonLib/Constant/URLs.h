ffe3a43b0d6c97cc64c98e8b48a0139a0a3f5587
yurongde <359067441@qq.com>
2016-01-31 16:12:50 +0800

    增加url

---
+++ CommonLib/Constant/URLs.h
@@ -0,0 +1,13 @@
+//
+//  URLs.h
+//  MainAppProgram
+//
+//  Created by yurongde on 16/1/31.
+//  Copyright © 2016年 yurongde. All rights reserved.
+//
+
+#ifndef URLs_h
+#define URLs_h
+
+#define my_URL @"http://www.baidu.com"
+#endif /* URLs_h */
